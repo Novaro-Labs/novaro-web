c91b23b342174ac263eeaf63069b246d6382ea8b
ningxzx <thinkxzx@gmail.com>
2024-10-25 17:46:42 +0800

    feat: 首页增加动画，修改登录态

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Navigate } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import Main from "../pages/main";
 import HomePage from "../pages/home-page";
 import LandingPage from "../pages/landing-page";
@@ -6,37 +6,46 @@ import ProfilePage from "../pages/profile-page";
 import SpacePage from "../pages/space-page";
 import TokenPage from "../pages/token-page";
 
+import { ReactNode } from "react";
+import { useAccount } from "wagmi";
+
+const PrivateRoute: React.FC<{ element: ReactNode }> = ({ element }) => {
+  const { isConnected } = useAccount();
+  return isConnected ? element : <Navigate to="/login" replace />;
+};
+
 const routes = [
-    {
-        path: '/login',
-        element: <LandingPage />,
-    },
-    {
-        path: '/',
-        element: <Main />,
-        children: [
-            {
-              path: '/',
-              element: <Navigate to="home" replace/>,
-            },
-            {
-                path: 'home',
-                element: <HomePage />,
-            },
-            {
-                path: 'profile',
-                element: <ProfilePage />,
-            },
-            {
-                path: 'space',
-                element: <SpacePage />,
-            },
-            {
-                path: 'token',
-                element: <TokenPage />,
-            },
-        ]
-    }
+  {
+    path: "/login",
+    element: <LandingPage />,
+  },
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to="home" replace />,
+      },
+      {
+        path: "home",
+        element: <PrivateRoute element={<HomePage />} />,
+      },
+      {
+        path: "profile",
+        element: <PrivateRoute element={<ProfilePage />} />,
+      },
+      {
+        path: "space",
+        element: <PrivateRoute element={<SpacePage />} />,
+      },
+      {
+        path: "token",
+        element: <PrivateRoute element={<TokenPage />} />,
+      },
+    ],
+  },
 ];
+// if wallet is not connected, redirect to login page
 
 export default createBrowserRouter(routes);
