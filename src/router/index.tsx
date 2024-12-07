14c6cd86167e09fa2904c383b59c2cd0deba96f9
GuanWhite <58129078+GuanWhite@users.noreply.github.com>
2024-12-07 14:24:34 +0800

    Update index.tsx
    add community router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -69,6 +69,10 @@ const routes = [
         path: "cryptos",
         element: <PrivateRoute element={<CryptosPage />} />,
       },
+      {
+        path: "community",
+        element: <PrivateRoute element={<CommunityPage />} />,
+      },
     ],
   },
   {
